66730ee66d37e63ce5689fac4b781224bf3efd4f
Matt Osborn <matthew.osborn@theguardian.com>
2015-02-27 11:46:46 +0000

    current term politicians

--- scripts/theyworkforyou/convert_to_csv.py
+++ scripts/theyworkforyou/convert_to_csv.py
@@ -1,14 +1,21 @@
 from xml.etree import ElementTree as ET
 import codecs
+import time, datetime
 
-members_xml = ET.parse('all-members.xml')
+members_xml = ET.parse('all-members-2010.xml')
 
 attrs = ['firstname','lastname', 'title', 'party', 'constituency', 'fromdate', 'todate', 'house']
 
 members = []
 for member in members_xml.findall('member'):
 	member = { attr: member.get(attr) for attr in attrs }
-	members.append(member)
+	try:
+		member['fromdate'] = int(time.mktime(datetime.datetime.strptime(member['fromdate'], "%Y-%m-%d").timetuple()))
+		member['todate'] = int(time.mktime(datetime.datetime.strptime(member['todate'], "%Y-%m-%d").timetuple()))
+		members.append(member)
+	except Exception, e:
+		print 'error', member
+		pass
 
 with codecs.open('members.csv', 'w', 'utf-8') as outfile:
 	outfile.write(','.join(attrs) + '\n')
